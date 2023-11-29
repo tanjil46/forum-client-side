877fa24a585bffddda18712ddb5049350449195b
tanjil hoassain <tanjilt46@gmail.com>
2023-11-29 01:37:40 -0700

    show annocement section changed

--- src/Homepages/ShowAnnonce.jsx
+++ src/Homepages/ShowAnnonce.jsx
@@ -1,23 +1,45 @@
-import { useState } from "react";
+
 import usePublicAxios from "../Hooks/usePublicAxios";
 import Hadlines from "../general componets/Hadlines";
 import AnnoncShowaftermap from "./AnnoncShowaftermap";
+import { useQuery } from "@tanstack/react-query";
 
 const ShowAnnonce = () => {
-const[annonces,setAnnounces]=useState([])
-    const openAxios=usePublicAxios()
-   openAxios.get('/annonce')
-   .then(res=>{
+
+     const openAxios=usePublicAxios()
+//    openAxios.get('/annonce')
+//    .then(res=>{
     
-    setAnnounces(res.data)
+//     setAnnounces(res.data)
+//    })
+
+
+
+   const{data:annonces=[]}=useQuery({
+    queryKey:['announc'],
+   
+    queryFn:async()=>{
+           const res=await openAxios.get('/annonce')
+          
+           return res.data
+    }
+
+
+
+
    })
+
+
+
+
+
 
     return (
         <div>
             <Hadlines title={'Admin Annoncements'}></Hadlines>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-6 my-12">
             {
-                annonces.map((ment,idx)=><AnnoncShowaftermap ment={ment} key={idx}></AnnoncShowaftermap>)
+                annonces?.map((ment,idx)=><AnnoncShowaftermap ment={ment} key={idx}></AnnoncShowaftermap>)
             }
 </div>
 
